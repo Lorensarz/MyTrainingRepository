82fb35a22517fc63fdac4febf8b369285519d1f9
Дмитрий Петров <www.lorens999@yandex.ru>
2021-09-02 01:04:26 +0300

    ДЗ 7.10

--- 07_NumbersAndDates/Date/src/main/java/Main.java
+++ 07_NumbersAndDates/Date/src/main/java/Main.java
@@ -1,3 +1,8 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+
 
 public class Main {
 
@@ -7,16 +12,51 @@ public class Main {
         int month = 12;
         int year = 1990;
 
+
+
         System.out.println(collectBirthdays(year, month, day));
 
     }
 
     public static String collectBirthdays(int year, int month, int day) {
 
+/**
+ 1. создать переменные класса LocalDate — birthday и today
+ 2. написать цикл в котором прибавлять 1 год к birthday и сравнивать дату с сегодняшней
+ используя метод isAfter или isBefore
+
+ Таким образом у вас будет проверяться не только год, но и день, что позволит точно
+ выводить данные о прошедших днях рождениях.
+ */
+
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy - EEE").localizedBy(
+                new Locale("us"));
+        String stringToday = today.format(formatter);
+
+
+        LocalDate birthday = LocalDate.of(1990, 12, 31);
+        String stringBirthday = birthday.format(formatter);
+
+
+        LocalDate start = LocalDate.parse(stringBirthday, formatter);
+        LocalDate end = LocalDate.parse(stringToday, formatter);
+
+
+        for (birthday = start; birthday.isBefore(end); birthday = birthday.plusYears(1)) {
+
+            System.out.println(formatter.format(birthday));
+
+        }
+
+
+
+
         //TODO реализуйте метод для построения строки в следующем виде
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
-        
+
+
         return "";
     }
 }
