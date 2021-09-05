626f7e61af5dc3a786c7b52306d084852bef16b0
Дмитрий Петров <www.lorens999@yandex.ru>
2021-09-05 23:20:02 +0300

    7.10 Задание 2

--- 07_NumbersAndDates/Periods/src/main/java/Main.java
+++ 07_NumbersAndDates/Periods/src/main/java/Main.java
@@ -1,13 +1,24 @@
-import java.time.LocalDate;
+import java.time.*;
+
 
 public class Main {
 
-  public static void main(String[] args) {
 
-  }
+    public static void main(String[] args) {
 
-  private static String getPeriodFromBirthday(LocalDate birthday) {
-    return "";
-  }
+        LocalDate birthday = LocalDate.of(1970, 1, 1);
+        LocalDate now = LocalDate.now();
+
+        getPeriodFromBirthday(birthday, now);
+
+    }
+
+
+    private static void getPeriodFromBirthday(LocalDate birthday, LocalDate now) {
+
+        Period difference = Period.between(birthday, now);
+        System.out.printf("%d years, %d months, %d days ",
+                difference.getYears(), difference.getMonths(), difference.getDays());
+    }
 
 }
