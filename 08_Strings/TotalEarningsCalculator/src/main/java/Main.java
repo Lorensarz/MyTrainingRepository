524a7277e0f945eaa32e94161cdea692d421930d
Дмитрий Петров <www.lorens999@yandex.ru>
2021-09-07 02:54:01 +0300

    8.9 Задание 1

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,48 @@
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
-  }
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+
+        System.out.println(getEarnings(text));
+
+        //TODO: напишите ваш код, результат вывести в консоль
+
+    }
+
+    public static int getEarnings (String text) {
+
+        //Strings
+
+        String earningsVasya = "заработал";
+        String earningsPetya = "Петя - ";
+        String earningsMasha = "Маша - ";
+
+        //Start
+
+        int startVasya = text.indexOf(earningsVasya) + earningsVasya.length();
+        int startPetya = text.indexOf(earningsPetya) + earningsPetya.length();
+        int startMasha = text.indexOf(earningsMasha) + earningsMasha.length();
+
+        //Ends
+
+        int endVasya = text.indexOf("рублей", startVasya);
+        int endPetya = text.indexOf("рубля", startPetya);
+        int endMasha = text.indexOf("рублей", startMasha);
+
+        //Substrings
+
+        String vasya = text.substring(startVasya, endVasya);
+        String petya = text.substring(startPetya, endPetya);
+        String masha = text.substring(startMasha, endMasha);
+
+        //Total earnings
+
+        int totalEarnings = Integer.parseInt(vasya.trim()) + Integer.parseInt(petya.trim()) + Integer.parseInt(masha.trim());
+
+        return totalEarnings;
+
+    }
+
 
 }
