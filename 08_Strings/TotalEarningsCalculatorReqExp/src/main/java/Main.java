c97c21fae3f8c281a483100f0c023d7491a7ac4e
Дмитрий Петров <www.lorens999@yandex.ru>
2021-09-20 01:54:45 +0300

    8.9 Задание 3

--- 08_Strings/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,14 +1,38 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        System.out.println(calculateSalarySum());
 
-  }
+    }
 
-  public static int calculateSalarySum(String text){
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+    public static int calculateSalarySum() {
+        String text = "Толя заработал 35000 рублей, а Лера - 45000 рублей";
 
-    //TODO: реализуйте метод
-    return -1;
-  }
+        Pattern pattern = Pattern.compile("(\\d+)");
+        Matcher matcher = pattern.matcher(text);
 
+        Integer sum = 0;
+
+        while (matcher.find()) {
+            sum += Integer.parseInt(matcher.group(1));
+
+        }
+
+        if (sum > 0) {
+            System.out.printf("%d\n", sum);
+        } else {
+            System.out.println("Никто ничего не заработал");
+
+        }
+
+        return -1;
+    }
 }
+//TODO: реализуйте метод
+
+
+
+
