6e0a5baca2e003d0b374dd0c829c2e56a2f787ea
Дмитрий Петров <www.lorens999@yandex.ru>
2021-09-20 00:51:43 +0300

    8.9 Задание 5

--- 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
+++ 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
@@ -4,34 +4,41 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static java.lang.String.format;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
 
-      Pattern pattern = Pattern.compile("[^\\d].*[а-яА-ЯёЁ]\\s+");
-      Matcher matcher = pattern.matcher(input);
 
-      String[] splitString = (input.split("\\s+"));
+            Pattern pattern = Pattern.compile(".*[\\Dа-яА-ЯёЁ]\\s+");
+            Matcher matcher = pattern.matcher(input);
 
-        if (matcher.find()) {
-          System.out.println("Фамилия: " + splitString[0] +
-                  "\nИмя: " + splitString[1] +
-                  "\nОтчество: " + splitString[2]);
-        } else {
-          System.out.println("Введенная строка не является ФИО");
+
+            String[] splitString = (input.split("\\s+"));
+
+            if (splitString.length != 3) {
+
+            }
+
+            if (matcher.find()) {
+                System.out.println("Фамилия: " + splitString[0] +
+                        "\nИмя: " + splitString[1] +
+                        "\nОтчество: " + splitString[2]);
+
+            } else {
+                System.out.println("Введенная строка не является ФИО");
+
+            }
+
+            //TODO:напишите ваш код тут, результат вывести в консоль.
+            //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
         }
-
-        //TODO:напишите ваш код тут, результат вывести в консоль.
-        //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
-      }
     }
-  }
+}
