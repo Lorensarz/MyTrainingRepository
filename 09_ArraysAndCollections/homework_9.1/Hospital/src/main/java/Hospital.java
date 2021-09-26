74832d831635f16597c47ef3342673c7dad8f945
Дмитрий Петров <www.lorens999@yandex.ru>
2021-09-26 14:02:08 +0300

    HomeWork_9.1 задание 2

--- 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
+++ 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
@@ -2,21 +2,44 @@ public class Hospital {
 
     public static float[] generatePatientsTemperatures(int patientsCount) {
 
-        //TODO: напишите метод генерации массива температур пациентов
+        //Напишите код метода generatePatientsTemperatures(), который должен сгенерировать массив float,
+        // содержащий значения температур пациентов от 32 до 40 градусов.
 
-        return new float[0];
+
+        float[] patientsTemperatures = new float[patientsCount];
+        for (int i = 0; i < patientsTemperatures.length; i++) {
+            patientsTemperatures[i] = 42 + (float) (32 * Math.random());
+        }
+        return patientsTemperatures;
     }
+
+
+    //TODO: напишите метод генерации массива температур пациентов
+
 
     public static String getReport(float[] temperatureData) {
         /*
         TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
             а также температуры всех пациентов.
+
         */
 
-        String report =
-                "Температуры пациентов: " + 0 +
-                        "\nСредняя температура: " + 0 +
-                        "\nКоличество здоровых: " + 0;
+        String patientsTemperatures = "";
+        float averageTemperature = 0;
+        int countHealthyPatients = 0;
+
+        for (float patients : temperatureData) {
+            patientsTemperatures += String.format("%.1f", patients) + " ";
+            averageTemperature += patients;
+            if (patients >= 36.2F && patients <= 36.9F)
+                countHealthyPatients++;
+        }
+
+
+        String report = "Температуры пациентов: " + patientsTemperatures +
+                        "\nСредняя температура: " +
+                String.format("%.2f", (averageTemperature / temperatureData.length)) +
+                        "\nКоличество здоровых: " + countHealthyPatients;
 
         return report;
     }
