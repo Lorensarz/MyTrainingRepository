71b09a0b184013a7caaeb1318a8b6d73c3005942
Дмитрий Петров <www.lorens999@yandex.ru>
2021-11-21 14:30:34 +0300

    HomeWork 10.3 on check

--- 10_InheritanceAndPolymorphism/homework_10.3/src/main/java/IndividualBusinessman.java
+++ 10_InheritanceAndPolymorphism/homework_10.3/src/main/java/IndividualBusinessman.java
@@ -1,2 +1,12 @@
 public class IndividualBusinessman extends Client {
+
+    @Override
+    public void put(double amountToPut) {
+        if (amountToPut < 1000) {
+            super.put(amountToPut - (amountToPut * 0.01));
+        }
+        if (amountToPut >= 1000) {
+            super.put(amountToPut - (amountToPut * 0.005));
+        }
+    }
 }
--- 10_InheritanceAndPolymorphism/homework_10.3/src/main/java/LegalPerson.java
+++ 10_InheritanceAndPolymorphism/homework_10.3/src/main/java/LegalPerson.java
@@ -1,2 +1,7 @@
 public class LegalPerson extends Client {
+
+    @Override
+    public void take(double amountToTake) {
+        super.take(amountToTake + (amountToTake * 0.01));
+    }
 }
