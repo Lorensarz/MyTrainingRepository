b02110d79b929777d8050c280839ccc53ec08bb0
Дмитрий Петров <www.lorens999@yandex.ru>
2022-01-16 20:50:38 +0300

    copyFolder

--- 13_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
@@ -1,5 +1,43 @@
+import java.io.*;
+
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
+
+    public static void copyFolder(String sourceDirectory, String destinationDirectory) throws IOException {
         // TODO: write code copy content of sourceDirectory to destinationDirectory
+
+        File src = new File(sourceDirectory);
+        File dst = new File(destinationDirectory);
+
+        if (src.isDirectory()) {
+            if (!dst.exists()) {
+                dst.mkdir();
+            }
+
+            String files[] = src.list();
+
+            for (String file : files) {
+                String srcFile = String.valueOf(new File(src, file));
+                String destFile = String.valueOf(new File(dst, file));
+
+                copyFolder(srcFile, destFile);
+            }
+
+        } else {
+            InputStream in = new FileInputStream(src);
+            OutputStream out = new FileOutputStream(dst);
+
+            byte[] buffer = new byte[1024];
+
+            int length;
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+
+            in.close();
+            out.close();
+        }
     }
 }
+
+
+
--- 13_FilesAndNetwork/homework_11.2/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_11.2/src/main/java/Main.java
@@ -1,4 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+           String input = scanner.nextLine();
+           if (input.equals("")) {
+               break;
+           }
+            System.out.println("Введите путь к папке: ");
+            FileUtils.copyFolder(input, input);
+        }
+
     }
 }
