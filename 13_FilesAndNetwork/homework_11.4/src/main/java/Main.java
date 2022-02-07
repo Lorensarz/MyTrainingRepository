92baf55a87e2131ae9550fed05f95bb1a13975f2
Дмитрий Петров <www.lorens999@yandex.ru>
2022-02-07 20:25:59 +0300

    11.4

--- 13_FilesAndNetwork/homework_11.4/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_11.4/src/main/java/Main.java
@@ -1,32 +1,67 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.File;
-import java.io.IOException;
-
 public class Main {
 
-    public static void main(String[] args) {
-        File input = new File("/path/directory");
+    private static String IMAGE_DESTINATION_FOLDER =
+            "C:\\images";
+
+    public static void main(String[] args) throws IOException {
+        String strURL = "https://lenta.ru";
+        Document document = Jsoup.connect(strURL).get();
+        Elements imageElements = document.select("img[src~=(?i)\\.(png|jpe?g|gif)]");
+
+        for(Element imageElement : imageElements){
+            String strImageURL = imageElement.attr("abs:src");
+            downloadImage(strImageURL);
+        }
+    }
+
+
+    private static String createPath() throws IOException {
+        Path path = Paths.get(IMAGE_DESTINATION_FOLDER);
+        if (!Files.exists(path)) {
+            Files.createDirectory(path);
+        }
+        return String.valueOf(path);
+    }
+
+
+    private static void downloadImage(String strImageURL){
+        String strImageName =
+                strImageURL.substring( strImageURL.lastIndexOf("/") + 1 );
+        System.out.println("Saving: " + strImageName + ", from: " + strImageURL);
+
         try {
-            Document doc = Jsoup.parse(input, "UTF-8", "https://lenta.ru");
+            URL urlImage = new URL(strImageURL);
+            InputStream in = urlImage.openStream();
 
-            Elements links = doc.select("a[href]"); // a with href
-            Elements pngs = doc.select("img[src$=.png]");
-            // img with src ending .png
+            byte[] buffer = new byte[4096];
+            int n = -1;
 
-            Element masthead = doc.select("div.masthead").first();
-            // div with class=masthead
+            OutputStream os =
+                    new FileOutputStream( createPath() + "/" + strImageName );
 
-            Elements resultLinks = doc.select("h3.r > a"); // direct a after h3
-            System.out.println(links);
+            while ( (n = in.read(buffer)) != -1 ){
+                os.write(buffer, 0, n);
+            }
+            os.close();
+            System.out.println("Image saved");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
 
     }
 }
